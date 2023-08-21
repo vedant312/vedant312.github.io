ffa40704434d3e169ffed4d38532d50202a7dcb6
vedant dangi <82332572+vedant312@users.noreply.github.com>
2023-08-21 12:22:33 +0530

    Delete trial.js

--- trial.js
+++
@@ -1,9 +0,0 @@
-var audio = new Audio("voice.mp3");
-// document.querySelector("#photo").addEventListener("click",handleClick);
-// function handleClick(){
-//     audio.play();
-// }
-$("#photo").on("mouseover" , function(){
-    audio.play();
-});
-
